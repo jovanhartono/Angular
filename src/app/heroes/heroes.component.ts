118c0787a5296a5d387672821b636ba711ab2835
JOVANUS HARTONO <U541853@bca>
2021-02-23 16:56:22 +0700

    angular

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import {MessageService} from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero? : Hero
+
+  onSelect(hero : Hero) : void{
+    this.selectedHero = hero;
+    this.messageService.add(`Heroes Component : Selected hero id = ${hero.id}`);
+  }
+
+  getHeroes() : void{
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  hero: Hero = {
+    id: 1,
+    name:'Windranger'
+  };
+
+  constructor(private heroService : HeroService, private messageService : MessageService) { 
+
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES : Hero[] = [
+    { id: 11,  name: 'Shadow Fiend'},
+    { id: 12,  name: 'Bloodseeker'},
+    { id: 13,  name: 'Enigma'},
+    { id: 14,  name: 'TideHunter'},
+    { id: 15,  name: 'Dark Seer'},
+    { id: 16,  name: 'Abaddon'},
+    { id: 17,  name: 'Nature Prophet'},
+    { id: 18,  name: 'Slark'},
+    { id: 19,  name: 'Zeus'},
+    { id: 20,  name: 'Witch Doctor'}
+]
